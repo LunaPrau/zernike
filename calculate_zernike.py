aa88defab192f0f3f29e948c9fa78ab36995070a
Marta Chronowska <mchrnwsk@gmail.com>
2025-01-23 14:31:15 +0000

    Works with input variables from specification config file; make html visualisation optional

--- calculate_zernike.py
+++ calculate_zernike.py
@@ -1,18 +1,32 @@
 import subprocess
-from check_output import check_zernike_output_main
+from zernike.convert_to_point_cloud import convert_to_point_cloud_main
+from zernike.visualise_zernike import visualise_zernike_main
 import os
+import logging
 
-execPath = "/home/mchrnwsk/tools/rosetta/source/bin/zernike.default.linuxgccrelease"
+def calculate_zernike_main(inputPath, outputDir, zernikeSettings):
+    execPath = "/home/mchrnwsk/tools/rosetta/source/bin/zernike.default.linuxgccrelease"
 
-pointCloudPath = "/home/mchrnwsk/tools/zernike_ligand/data/palmitic_acid_conf_out_7.csv"
-name = (os.path.splitext(os.path.basename(pointCloudPath))[0])
+    makeHtml = zernikeSettings["make_html"]
+    gridSize = zernikeSettings["grid_size"]
 
-outputDir = "/home/mchrnwsk/tools/zernike_ligand/output"
-outputDescr = f"{outputDir}/{name}_descrOut.csv"
-outputVoxel = f"{outputDir}/{name}_voxelOut.json"
-outputHtml = f"{outputDir}/{name}_voxelGrid.html"
+    name, ext = (os.path.splitext(os.path.basename(inputPath)))
+    pointCloudPath = convert_to_point_cloud_main(inputPath, ext)
+    outputDescr = f"{outputDir}/{name}_descrOut.csv"
+    outputVoxel = f"{outputDir}/{name}_voxelOut.json"
+    outputHtml = None
 
-command = f"{execPath} -xyzfile {pointCloudPath} -descriptor_out {outputDescr} -voxelgrid_out {outputVoxel}"
-subprocess.run(command, shell=True, check=True)
+    command = f"{execPath} -xyzfile {pointCloudPath} -descriptor_out {outputDescr} -voxelgrid_out {outputVoxel}"
+    result = subprocess.run(command, shell=True, check=True, capture_output = True, text = True)
+    logging.debug(result.stdout)
+    logging.warning(result.stderr)
+    logging.debug("*** Zernike calculation complete!")
+    logging.debug(f"Output saved to: {outputDescr} and {outputVoxel}")
 
-check_zernike_output_main(outputDescr, outputVoxel, outputHtml)
+    if makeHtml:
+        outputHtml = f"{outputDir}/{name}_voxelGrid.html"
+        visualise_zernike_main(outputVoxel, outputHtml, gridSize)
+    return {"descr": outputDescr, "voxel": outputVoxel, "html": outputHtml}
+
+if __name__ == "__main__":
+    calculate_zernike_main()
