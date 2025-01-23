a982be5d31554f23b909345fed1b0ef3e198d46f
Marta Chronowska <mchrnwsk@gmail.com>
2025-01-23 08:54:05 +0000

    Make into a callable module script

--- check_output.py
+++ check_output.py
@@ -1,15 +1,17 @@
 from voxel_to_html import voxel_to_html
 import numpy as np
 
-# read in zernike descriptors
-desc = np.loadtxt("data/descriptor.csv", skiprows=1)
+def check_zernike_output_main(decrPath, voxelPath, outputHtml):
 
-# euclidian distance (compare to itself, should have another one)
-dist = np.linalg.norm(desc - desc)
+    # read in zernike descriptors
+    desc = np.loadtxt(decrPath, skiprows=1)
 
-# Output Voxelgrid
-voxel_file = "data/voxelgrid.json"
-grid_size = 64
-html_file = "data/voxelgrid.html"
-voxel_to_html(voxel_file, grid_size, html_file)
+    # euclidian distance (compare to itself, should have another one)
+    dist = np.linalg.norm(desc - desc)
 
+    # Output Voxelgrid
+    gridSize = 64
+    voxel_to_html(voxelPath, gridSize, outputHtml)
+
+if __name__ == "__main__":
+    check_zernike_output_main()
