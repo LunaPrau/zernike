35c602240794e294802f57454fb13aa6384c912a
Marta Chronowska <mchrnwsk@gmail.com>
2025-01-24 14:21:59 +0000

    Works with list (batch) or single (oneByOne) xyz input, detected based on .txt extension

--- calculate_zernike.py
+++ calculate_zernike.py
@@ -3,30 +3,46 @@ from zernike.convert_to_point_cloud import convert_to_point_cloud_main
 from zernike.visualise_zernike import visualise_zernike_main
 import os
 import logging
+import time
 
 def calculate_zernike_main(inputPath, outputDir, zernikeSettings):
+    startTime = time.time()
     execPath = "/home/mchrnwsk/tools/rosetta/source/bin/zernike.default.linuxgccrelease"
 
     makeHtml = zernikeSettings["make_html"]
     gridSize = zernikeSettings["grid_size"]
 
     name, ext = (os.path.splitext(os.path.basename(inputPath)))
-    pointCloudPath = convert_to_point_cloud_main(inputPath, ext)
-    outputDescr = f"{outputDir}/{name}_descrOut.csv"
-    outputVoxel = f"{outputDir}/{name}_voxelOut.json"
+    if ext == ".txt":
+        xyzArg = "-xyzlist"
+        pointCloudPath = inputPath
+        outputDescr = f"{outputDir}/{name}_descrOut.csv"
+        outputVoxel = f"{outputDir}/{name}_voxelOut.json"
+    else:
+        xyzArg = "-xyzfile"
+        pointCloudPath = convert_to_point_cloud_main(inputPath, ext)
+        outputDescr = f"{outputDir}/{name}_descrOut.csv"
+        outputVoxel = f"{outputDir}/{name}_voxelOut.json"
     outputHtml = None
 
-    command = f"{execPath} -xyzfile {pointCloudPath} -descriptor_out {outputDescr} -voxelgrid_out {outputVoxel}"
+    if makeHtml:
+        command = f"{execPath} {xyzArg} {pointCloudPath} -descriptor_out {outputDescr} -voxelgrid_out {outputVoxel} -never_leave_neighbour 1"
+    else:
+        command = f"{execPath} {xyzArg} {pointCloudPath} -descriptor_out {outputDescr} -never_leave_neighbour 1"
     result = subprocess.run(command, shell=True, check=True, capture_output = True, text = True)
     logging.debug(result.stdout)
-    logging.warning(result.stderr)
+    # Log standard error, filter out empty lines
+    for line in result.stderr.splitlines():
+        if line.strip():  # Check for non-empty lines
+            logging.warning(line)
     logging.debug("*** Zernike calculation complete!")
     logging.debug(f"Output saved to: {outputDescr} and {outputVoxel}")
 
     if makeHtml:
         outputHtml = f"{outputDir}/{name}_voxelGrid.html"
         visualise_zernike_main(outputVoxel, outputHtml, gridSize)
-    return {"descr": outputDescr, "voxel": outputVoxel, "html": outputHtml}
 
+    elapsedTime = time.time() - startTime
+    return elapsedTime, outputDescr
 if __name__ == "__main__":
     calculate_zernike_main()
